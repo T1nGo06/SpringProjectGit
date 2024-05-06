f50a1657a188d845a28b859bc36f2069c3c544bb
Talant <talantasanov2006@gmail.com>
2024-05-06 20:51:32 +0600

    My first commit

---
+++ src/test/java/com/talant/springprojectgit/SpringProjectGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talant.springprojectgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
