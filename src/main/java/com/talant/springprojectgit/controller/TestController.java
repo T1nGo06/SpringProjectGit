9ffe7a78e7641a236c0d70077e7b0a75c45c735e
Talant <talantasanov2006@gmail.com>
2024-05-08 20:17:20 +0600

    commit-1

--- src/main/java/com/talant/springprojectgit/controller/TestController.java
+++ src/main/java/com/talant/springprojectgit/controller/TestController.java
@@ -1,2 +1,14 @@
-package com.talant.springprojectgit.controller;public class TestController {
+package com.talant.springprojectgit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TestController {
+    @GetMapping(value = "/test-1")
+    public String sayHello(){
+        return "Hello World!";
+    }
 }
--- src/test/java/com/talant/springprojectgit/controllerstest/TestControllerTest.java
+++ src/test/java/com/talant/springprojectgit/controllerstest/TestControllerTest.java
@@ -1,15 +1,38 @@
-package com.talant.springprojectgit.controllers;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/api")
-public class TestController {
-
-    @GetMapping(value = "/test-1")
-    public String sayHello(){
-        return "Hello, World!";
-    }
-}
+//package com.talant.springprojectgit.controllerstest;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//
+//@RestController
+//@RequestMapping(value = "/api")
+//public class TestControllerTest {
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    void testSayHello_OK(){
+//        mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:" + this.port + "/api/test-1")
+//                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(js)
+//
+//    }
+//
+//    public String sayHello(){
+//        return "Hello, World!";
+//    }
+//}
