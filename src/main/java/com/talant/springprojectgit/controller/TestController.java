85e0c0c1f53a581a812147bcab15554676a0df53
Talant <talantasanov2006@gmail.com>
2024-05-08 20:13:50 +0600

    Initial comit

---
+++ src/main/java/com/talant/springprojectgit/controller/TestController.java
@@ -0,0 +1,2 @@
+package com.talant.springprojectgit.controller;public class TestController {
+}
---
+++ src/test/java/com/talant/springprojectgit/controllerstest/TestControllerTest.java
@@ -0,0 +1,15 @@
+package com.talant.springprojectgit.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TestController {
+
+    @GetMapping(value = "/test-1")
+    public String sayHello(){
+        return "Hello, World!";
+    }
+}
